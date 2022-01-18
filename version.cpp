be9329a0e10da2d80ee7d7c680bd7581cb0f411d
wianoskki <thirafi.wian@gmail.com>
2022-01-18 12:22:45 +0700

    chore: working on newest tf model

--- version.cpp
+++ version.cpp
@@ -1,4 +1,4 @@
-const char* GIT_REV="f9c19d4";
+const char* GIT_REV="08a1bfb";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="main";
-const char* BUILD_TIME="2022-01-03 15:34";
+const char* BUILD_TIME="2022-01-18 12:12";
